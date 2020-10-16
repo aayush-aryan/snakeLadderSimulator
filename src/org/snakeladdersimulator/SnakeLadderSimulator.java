92ad984da355e22e82dfd2981ec16ab5ac0ecf2c
aayush-aryan <log2aayusharyan@gmail.com>
2020-10-16 22:59:27 +0530

    [aayush]added. welcome message for the Snake ladder Program

---
+++ src/org/snakeladdersimulator/SnakeLadderSimulator.java
@@ -0,0 +1,10 @@
+package org.snakeladdersimulator;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class SnakeLadderSimulator {
+    public static void main(String[] args) {
+        System.out.println("Welcome to SnakeLadderSimulatorProgram");
+    }
+
+}
