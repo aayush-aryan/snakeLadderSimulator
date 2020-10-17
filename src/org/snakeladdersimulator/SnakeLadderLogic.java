7a76f04239fa27c3c79abcf2390e7eebe2513ca9
aayush-aryan <log2aayusharyan@gmail.com>
2020-10-17 12:26:13 +0530

    [aayush]added. a method for rolling dice

--- src/org/snakeladdersimulator/SnakeLadderLogic.java
+++ src/org/snakeladdersimulator/SnakeLadderLogic.java
@@ -1,5 +1,15 @@
 package org.snakeladdersimulator;
 
+import java.util.Random;
+
 public class SnakeLadderLogic {
     int player1=0;
+
+    public int rollDice()
+    {
+        int dice = 0;
+        Random random = new Random();
+        dice=random.nextInt(7);
+        return (dice==0?1:dice);
+    }
 }
